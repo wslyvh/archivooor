d571111f93dbbced924ae21b48547272e2a905ca
wslyvh <wslyvh@users.noreply.github.com>
2023-06-17 00:16:28 +0200

    seo

--- src/components/layout/Seo.tsx
+++ src/components/layout/Seo.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { SITE_DESCRIPTION, SITE_NAME, SITE_URL, SOCIAL_TWITTER } from 'utils/config'
-import { DefaultSeo } from 'next-seo'
+import { NextSeo } from 'next-seo'
 
 export function Seo() {
   const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : SITE_URL
 
   return (
     <>
-      <DefaultSeo
+      <NextSeo
         title={SITE_NAME}
         defaultTitle={SITE_NAME}
         titleTemplate={`%s | ${SITE_NAME}`}
         description={SITE_DESCRIPTION}
-        defaultOpenGraphImageWidth={1200}
-        defaultOpenGraphImageHeight={630}
         openGraph={{
           type: 'website',
           siteName: SITE_NAME,
@@ -24,6 +22,8 @@ export function Seo() {
               alt: `${SITE_NAME} Open Graph Image`,
             },
           ],
+          defaultImageWidth: 1200,
+          defaultImageHeight: 630,
         }}
         twitter={{
           handle: `@${SOCIAL_TWITTER}`,
